3441d076f499c179c103b4efd2898f20e94b39b3
Bryan Yung <byung806@gmail.com>
2022-02-05 15:09:42 -0500

    Deselect selected piece after clicking it

--- src/listener/ClickListener.java
+++ src/listener/ClickListener.java
@@ -50,6 +50,9 @@ public class ClickListener extends MouseAdapter {
                     return;
                 } else {
                     board.removeHighlightedSquare(start);
+                    if (start == squareId) {
+                        piece = null;
+                    }
                 }
             }
             board.setSelectedPiece(piece);
